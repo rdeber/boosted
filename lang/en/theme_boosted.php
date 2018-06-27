3b6f3f845f8050d1880bbe5ecbda3f3a143a892d
Ryan D <rdeber@gmail.com>
2018-06-27 16:59:35 -0400

    Added config and settings files.

--- lang/en/theme_boosted.php
+++ lang/en/theme_boosted.php
@@ -28,5 +28,31 @@ defined('MOODLE_INTERNAL') || die();
 $string['choosereadme'] = 'Boosted is a child theme of Boost.';
 // The name of our plugin.
 $string['pluginname'] = 'Boosted';
-// We need to include a lang string for each block region.
+
+// Default strings from boost.
+$string['advancedsettings'] = 'Advanced settings';
+$string['backgroundimage'] = 'Background image';
+$string['backgroundimage_desc'] = 'The image to display as a background of the site. The background image you upload here will override the background image in your theme preset files.';
+$string['brandcolor'] = 'Brand colour';
+$string['brandcolor_desc'] = 'The accent colour.';
+$string['bootswatch'] = 'Bootswatch';
+$string['bootswatch_desc'] = 'A bootswatch is a set of Bootstrap variables and css to style Bootstrap';
+$string['currentinparentheses'] = '(current)';
+$string['configtitle'] = 'Boost';
+$string['fontsize'] = 'Theme base fontsize';
+$string['fontsize_desc'] = 'Enter a fontsize in %';
+$string['generalsettings'] = 'General settings';
+$string['nobootswatch'] = 'None';
+$string['presetfiles'] = 'Additional theme preset files';
+$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href=https://docs.moodle.org/dev/Boost_Presets>Boost presets</a> for information on creating and sharing your own preset files, and see the <a href=http://moodle.net/boost>Presets repository</a> for presets that others have shared.';
+$string['preset'] = 'Theme preset';
+$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
+$string['privacy:metadata'] = 'The Boost theme does not store any personal data about any user.';
+$string['rawscss'] = 'Raw SCSS';
+$string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will be injected at the end of the style sheet.';
+$string['rawscsspre'] = 'Raw initial SCSS';
+$string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
 $string['region-side-pre'] = 'Right';
+$string['privacy:metadata:preference:draweropennav'] = 'The user\'s preference for hiding or showing the drawer menu navigation.';
+$string['privacy:drawernavclosed'] = 'The current preference for the navigation drawer is closed.';
+$string['privacy:drawernavopen'] = 'The current preference for the navigation drawer is open.';
